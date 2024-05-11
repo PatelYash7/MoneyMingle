9d07db6ec20e6edd38cff22919a043042f6907a4
Yash Patel <109963122+PatelYash7@users.noreply.github.com>
2024-05-11 19:00:23 +0530

    Added Transactions for webhook call

--- apps/bank-webhook/src/index.ts
+++ apps/bank-webhook/src/index.ts
@@ -3,15 +3,53 @@ import db from "@moneymingle/db/client"
 import { paymentInformationZod } from "@moneymingle/zod/zod";
 const app = express();
 
-app.get("/bankHook", (req, res) => {
+app.get("/bankHook",async (req, res) => {
   const isValid = paymentInformationZod.safeParse(req.body);
 
   if (isValid) {
     const paymentInformation = {
       token: req.body.token,
-      userId: req.body.userId,
+      userId: req.body.user_Identifier,
       amount: req.body.amount,
     };
+    try {
+        await db.$transaction([
+            db.balance.update({
+                where:{
+                    userId:paymentInformation.userId
+                },
+                data:{
+                    amount:{
+                        increment:Number(paymentInformation.amount)
+                    }
+                }
+            }),
+            db.onRampTransaction.update({
+                where:{
+                    token:paymentInformation.token
+                },
+                data:{
+                    status:"SUCCESS"
+                }
+            })
+        ]);
+        res.json({
+            message:"Captured"
+        })
+    } catch (err) {
+        console.log(err)
+        await db.onRampTransaction.update({
+            where:{
+                token:paymentInformation.token
+            },
+            data:{
+                status:"FAILED"
+            }
+        })
+        res.status(411).json({
+            message:"Error while processing the Transaction"
+        })
+    }
   }else {
     res.status(400).json({
       message: "Invalid payment information",
