6cb20eb70c540ccef2e1f31e956d811de01d5bfd
Yash Patel <109963122+PatelYash7@users.noreply.github.com>
2024-05-14 12:18:14 +0530

    Created Transfer page

--- apps/user-client/app/(dashboard)/transfer/page.tsx
+++ apps/user-client/app/(dashboard)/transfer/page.tsx
@@ -1,5 +1,58 @@
-export default function(){
-    return <div>
-        Hello
+import { getServerSession } from "next-auth";
+import { authOptions } from "../../lib/auth";
+import prisma from "@moneymingle/db/client";
+import { time } from "console";
+import { stat } from "fs";
+import { AddMoneyCard } from "../../../components/AddMoneyCard";
+import { BalanceCard } from "../../../components/BalanceCard";
+import { OnRampTransactions } from "../../../components/OnRampTransaction";
+
+async function getBalance() {
+  const session = await getServerSession(authOptions);
+  const balance = await prisma.balance.findFirst({
+    where: {
+      id: session?.user?.id,
+    },
+  });
+  return {
+    amount: balance?.amount || 0,
+    locked: balance?.locked || 0,
+  };
+}
+async function getOnRampTransaction() {
+  const session = await getServerSession(authOptions);
+  const txn = await prisma.onRampTransaction.findMany({
+    where: {
+      userId: session?.user?.id,
+    },
+  });
+  return txn.map((t) => ({
+    time: t.startTime,
+    amount: t.amount,
+    status: t.status,
+    provider: t.provider,
+  }));
+}
+
+export default async function () {
+  const balance = await getBalance();
+  const transactions = await getOnRampTransaction();
+  return (
+    <div className="w-screen">
+      <div className="text-4xl text-[#6a51a6] pt-8 mb-8 font-bold">
+        Transfer
+      </div>
+      <div className="grid grid-cols-1 gap-4 p-4 md:grid-cols-2">
+        <div>
+          <AddMoneyCard />
+        </div>
+        <div>
+          <BalanceCard amount={balance.amount} locked={balance.locked} />
+          <div className="pt-4">
+            <OnRampTransactions transactions={transactions} />
+          </div>
+        </div>
+      </div>
     </div>
+  );
 }
