012ac73d666784d3c94da35840f1c2224674bf3b
Yash Patel <109963122+PatelYash7@users.noreply.github.com>
2024-05-21 00:56:02 +0530

    Created p2p Transfer section

--- apps/user-client/app/lib/actions/createOnRampTransctns.ts
+++ apps/user-client/app/lib/actions/createOnRampTransctns.ts
@@ -3,7 +3,6 @@
 import { getServerSession } from "next-auth"
 import { authOptions } from "../authoptions/auth"
 import db from "@moneymingle/db/client"
-import { OnRampTransactions } from "../../../components/OnRampTransaction";
 
 
 export async function createOnRampTransctns(amount:number,provider:string){
--- apps/user-client/components/SendCard.tsx
+++ apps/user-client/components/SendCard.tsx
@@ -2,25 +2,25 @@
 
 import { Button } from "@moneymingle/ui/button";
 import { Card } from "@moneymingle/ui/card";
-import { Center } from "@moneymingle/ui/center";
 import { TextInput } from "@moneymingle/ui/textinput";
 import { useState } from "react";
+import { p2pTransfer } from "../app/lib/actions/p2pTransfer";
 
 export function SendCard() {
-    const [number, setNumber] = useState("");
-    const [amount, setAmount] = useState("");
+    const[number,SetNumber]= useState("")
+    const[amount,SetAmount]= useState("")
 
     return <Card title="Send" bg=" bg-purple-200">
                 <div className="pt-2 min-w-72">
                     <TextInput placeholder={"Number"} label="Number" onChange={(value) => {
-                        setNumber(value)
+                        SetNumber(value)
                     }} />
                     <TextInput placeholder={"Amount"} label="Amount" onChange={(value) => {
-                        setAmount(value)
+                        SetAmount(value)
                     }} />
                     <div className="flex justify-center pt-4">
-                        <Button onClick={() => {
-
+                        <Button onClick={async () => {
+                            await p2pTransfer(number,Number(amount)*100)
                         }}>Send</Button>
                     </div>
                 </div>
