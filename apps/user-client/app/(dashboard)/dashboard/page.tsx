c4b1c5cd810f7eff8dc20c037cbaa9d6967e757c
Yash Patel <109963122+PatelYash7@users.noreply.github.com>
2024-06-02 23:11:21 +0530

    Styling Changed

--- apps/user-client/app/(dashboard)/dashboard/page.tsx
+++ apps/user-client/app/(dashboard)/dashboard/page.tsx
@@ -4,8 +4,8 @@ export default async function () {
   const greet = getGreeting();
   return (
     <>
-      <div className="pt-8 mb-6 text-4xl font-bold text-blueMain">{greet}</div>
-      <div className="grid grid-cols-3 gap-4">
+      <div className="pt-8 text-4xl font-bold text-blueMain">{greet}</div>
+      <div className="grid grid-cols-3 gap-4 my-8">
         <div className="grid col-span-2 bg-red-200 ">Hello</div>
         <div className="bg-green-200">jaihind</div>
       </div>
