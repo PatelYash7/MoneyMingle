c01b1aea72387760208fa2998c8772720f373464
Yash Patel <109963122+PatelYash7@users.noreply.github.com>
2024-06-12 00:34:34 +0530

    portfolio updated

--- apps/user-client/components/PortfolioCard.tsx
+++ apps/user-client/components/PortfolioCard.tsx
@@ -1,4 +1,8 @@
+'use client'
+import { useRouter } from "next/navigation"
+
 export const PortfolioCard = ()=>{
+    const router = useRouter()
     return <div className="px-4 py-2 bg-white rounded-lg">
         <div className="text-lg font-bold">
             <div>
@@ -10,7 +14,7 @@ export const PortfolioCard = ()=>{
         </div>
         <div className="flex items-center justify-center py-4 my-8 border-t-2">
             <div className="flex gap-4 px-4 py-4 ">
-                <button className="px-4 py-2 font-bold text-black bg-green-400 rounded-xl">Send</button>
+                <button onClick={()=>{router.replace('/p2p')}} className="px-4 py-2 font-bold text-black bg-green-400 rounded-xl">Send</button>
                 <button  className="px-4 py-2 font-bold text-black bg-red-400 rounded-xl">Receive</button>
                 <button  className="px-4 py-2 font-bold text-black bg-blue-400 rounded-xl">Deposit</button>
                 <button  className="px-4 py-2 font-bold text-black bg-gray-400 rounded-xl">Debit</button>
