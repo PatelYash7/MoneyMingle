66bc6f107614026d12b6e23b0947195e40ea3ed9
Yash Patel <109963122+PatelYash7@users.noreply.github.com>
2024-06-11 23:40:54 +0530

    Updated the Add money Card

--- apps/user-client/components/PortfolioCard.tsx
+++ apps/user-client/components/PortfolioCard.tsx
@@ -1,25 +1,19 @@
 export const PortfolioCard = ()=>{
-    return <div className="px-4 py-2 bg-white rounded-md">
+    return <div className="px-4 py-2 bg-white rounded-lg">
         <div className="text-lg font-bold">
             <div>
                 Portfolio
             </div>
-            <div className="text-5xl font-semibold text-gray-700 ">
+            <div className="text-5xl font-bold text-gray-700 ">
                 $0.00
             </div>
         </div>
-        <div className="flex items-center justify-center py-8 ">
-            <div className="flex gap-4 px-4 py-4 border-r-2 border-gray-950">
-                <button>Send</button>
-                <button>Receive</button>
-                <button>Receive</button>
-                <button>Receive</button>
-            </div>
+        <div className="flex items-center justify-center py-4 my-8 border-t-2">
             <div className="flex gap-4 px-4 py-4 ">
-                <button>Send</button>
-                <button>Send</button>
-                <button>Send</button>
-                <button>Receive</button>
+                <button className="px-4 py-2 font-bold text-black bg-green-400 rounded-xl">Send</button>
+                <button  className="px-4 py-2 font-bold text-black bg-red-400 rounded-xl">Receive</button>
+                <button  className="px-4 py-2 font-bold text-black bg-blue-400 rounded-xl">Deposit</button>
+                <button  className="px-4 py-2 font-bold text-black bg-gray-400 rounded-xl">Debit</button>
             </div>
         </div>
     </div>
